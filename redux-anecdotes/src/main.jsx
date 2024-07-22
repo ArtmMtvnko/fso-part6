6efc61afeceef1a67657a4e42f0b7c0c98479bff
Artem <artemm3note@gmail.com>
2024-07-22 19:24:08 +0300

    Ex. 6.9 has been done. Added filtering for anecdotes via redux store (redusers, actions, dispatch)

--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -1,5 +1,21 @@
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import filterReducer from './reducers/filterReducer'
+import anecdotesReducer from './reducers/anecdoteReducer'
+
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+
+const reducer = combineReducers({
+    anecdotes: anecdotesReducer,
+    filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
